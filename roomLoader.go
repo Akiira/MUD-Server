debd9b1621dfceb6cb55a9bdbd9e3de655bc1cfc
Randall Hudson <Hudsonrj09@gmail.com>
2015-03-07 18:54:31 -0500

    Created a new load rooms function that uses the xml encoding to make the process much easier and less error prone

--- roomLoader.go
+++ roomLoader.go
@@ -2,14 +2,11 @@
 package main
 
 import (
-	"strings"
+	//"strings"
 	"io/ioutil"
-	"fmt"
-)
-import (
-	_ "fmt"
-	_ "os"
-	_ "strings"
+	//"fmt"
+	"encoding/xml"
+	"os"
 )
 
 const (
@@ -18,30 +15,71 @@ const (
 	ROOM_EXITS = 2
 )
 
+type ExitXML struct {
+	XMLName xml.Name `xml:"Exit"`
+	Direction string `xml:"Direction"`
+	ConnectedRoomID int `xml:"RoomID"`
+}
 
-func LoadRooms() [4]*Room {
-	bytes, err := ioutil.ReadFile("testRooms1.txt")
+type RoomXML struct {
+	XMLName xml.Name `xml:"Room"`
+	ID int `xml:"ID"`
+	Name string `xml:"Name"`
+	Description string `xml:"Description"`
+	Exits []ExitXML `xml:"Exit"`
+}
+
+type RoomsXML struct {
+	XMLName xml.Name `xml:"Rooms"`
+	Rooms []RoomXML `xml:"Room"`
+}
+
+func loadRooms() [4]*Room {
+	xmlFile, err := os.Open("roomData.xml")
 	checkError(err)
-	var rooms [4]*Room
-	
-	foo := string(bytes)
-	
-	//fmt.Println(foo)
-	
-	roomsString := strings.Split(foo, "#R\r\n")
+	defer xmlFile.Close()
 
-	for index,element := range roomsString {
-		roomStr := strings.Split(element, "#\r\n")
-		fmt.Println(roomStr[0])
-		//fmt.Println(roomStr[1])
-		//fmt.Println(roomStr[2])
-		
-		rooms[index] = newRoom(roomStr[ROOM_ID], roomStr[ROOM_DESCRIPTION], roomStr[ROOM_EXITS])
+	XMLdata, _ := ioutil.ReadAll(xmlFile)
+
+	var roomsData RoomsXML
+    xml.Unmarshal(XMLdata, &roomsData)
+	
+	var rooms [4]*Room
+
+	for index, roomData := range roomsData.Rooms {
+		rooms[index] = newRoomFromXML(roomData)
 	}
 	
 	for index := range rooms {
 		rooms[index].setRoomLink(rooms)
 	}
-	
+
 	return rooms
 }
+
+//func LoadRooms() [4]*Room {
+//	bytes, err := ioutil.ReadFile("testRooms1.txt")
+//	checkError(err)
+//	var rooms [4]*Room
+	
+//	foo := string(bytes)
+	
+//	//fmt.Println(foo)
+	
+//	roomsString := strings.Split(foo, "#R\r\n")
+
+//	for index,element := range roomsString {
+//		roomStr := strings.Split(element, "#\r\n")
+//		fmt.Println(roomStr[0])
+//		//fmt.Println(roomStr[1])
+//		//fmt.Println(roomStr[2])
+		
+//		rooms[index] = newRoom(roomStr[ROOM_ID], roomStr[ROOM_DESCRIPTION], roomStr[ROOM_EXITS])
+//	}
+	
+//	for index := range rooms {
+//		rooms[index].setRoomLink(rooms)
+//	}
+	
+//	return rooms
+//}
