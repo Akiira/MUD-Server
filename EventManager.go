8fa19e33da6aceb0cbd543027e358ce6667d8b29
Randall Hudson <Hudsonrj09@gmail.com>
2015-03-08 16:12:37 -0400

    added a function in event manager to execute a players move command

--- EventManager.go
+++ EventManager.go
@@ -1,7 +1,27 @@
 // EventManager
 package main
 
+import (
+
+)
+
 type EventManager struct {
 	//a Q of events
 	//a timer
+}
+
+
+func executeMove(charName string, direction string) string {
+	char := onlinePlayers[charName]
+	room := worldRoomsG[char.RoomIN]
+	dirAsInt := convertDirectionToInt(direction)
+
+	if ( room.Exits[dirAsInt] >= 0 ) {
+		room.removePCFromRoom(charName)
+		room.ExitLinksToRooms[dirAsInt].addPCToRoom(charName)
+		char.RoomIN = room.Exits[dirAsInt]
+		return room.ExitLinksToRooms[dirAsInt].getFormattedOutput()
+	} else {
+		return "No exit in that direction"
+	}
 }
