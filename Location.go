ebd3fba83c6f50bdc900858223152ba8c70a0753
Randall Hudson <Hudsonrj09@gmail.com>
2015-03-09 12:23:45 -0400

    added note about this class

--- Location.go
+++ Location.go
@@ -1,5 +1,11 @@
 package main
 
+//TODO So a room can be uniquely identified by its roomID
+//		which is an int so this class may not be needed.
+//		Its still not clear how we are going to distinguish
+//		rooms on different servers though. It could be we use
+//		a struct like this with the fields Server and roomID.
+
 type Location struct {
 	x int
 	y int
