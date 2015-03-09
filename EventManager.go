6c967505cb598ac35d541c53c02688b936b77cb7
Randall Hudson <Hudsonrj09@gmail.com>
2015-03-08 21:13:49 -0400

    wrote simple executeAttack function.

--- EventManager.go
+++ EventManager.go
@@ -32,8 +32,29 @@ func executeMove(charName string, direction string) []FormattedString {
 	}
 }
 
-func executeStandardAttack(charName string, target string) {
+func executeStandardAttack(charName string, targetName string) []FormattedString {
+	char := onlinePlayers[charName]
+	target := worldRoomsG[char.RoomIN].getMonster(targetName)
+	output := make([]FormattedString, 2, 2)
 	
+	a1 := char.getAttack()
+	if(a1 >= target.Defense) {
+		target.HP -= 2
+		output[0].Value = "\nYou hit the " + targetName + "!"
+	} else {
+		output[0].Value = "\nYou missed the " + targetName + "!"
+	}
+	
+	a2 := target.getAttackRoll()
+	
+	if( a2 >= char.Defense) {
+		char.HitPoints -= 1
+		output[1].Value = "\nThe " + targetName + " hit you!"
+	} else {
+		output[1].Value = "\nThe " + targetName + " narrowly misses you!"
+	}
+	
+	return output
 }
 
 
