d157cd2f7a1e1a83afc33def2bfcc4bee7bd1955
Randall Hudson <Hudsonrj09@gmail.com>
2015-03-09 19:54:29 -0400

    changed wear location to a string. an example value would be "head" or "feet"

--- Armour.go
+++ Armour.go
@@ -4,11 +4,11 @@ package main
 type Armour struct {
 	Item
 	defense int
-	wearLocation int
+	wearLocation string
 }
 
 //When should constructors return a pointer instead of the object itself?
-func newArmour(name1 string, descr string, def int, wearLoc int) Armour {
+func newArmour(name1 string, descr string, def int, wearLoc string) Armour {
 	a := Armour{defense: def, wearLocation: wearLoc}
 	a.name = name1
 	a.description = descr
