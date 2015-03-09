f3a4796eb2bc9088da83148a3ac5e70e859e1526
Randall Hudson <Hudsonrj09@gmail.com>
2015-03-09 12:14:45 -0400

    removed old load rooms method

--- roomLoader.go
+++ roomLoader.go
@@ -57,29 +57,3 @@ func loadRooms() [4]*Room {
 	return rooms
 }
 
-//func LoadRooms() [4]*Room {
-//	bytes, err := ioutil.ReadFile("testRooms1.txt")
-//	checkError(err)
-//	var rooms [4]*Room
-	
-//	foo := string(bytes)
-	
-//	//fmt.Println(foo)
-	
-//	roomsString := strings.Split(foo, "#R\r\n")
-
-//	for index,element := range roomsString {
-//		roomStr := strings.Split(element, "#\r\n")
-//		fmt.Println(roomStr[0])
-//		//fmt.Println(roomStr[1])
-//		//fmt.Println(roomStr[2])
-		
-//		rooms[index] = newRoom(roomStr[ROOM_ID], roomStr[ROOM_DESCRIPTION], roomStr[ROOM_EXITS])
-//	}
-	
-//	for index := range rooms {
-//		rooms[index].setRoomLink(rooms)
-//	}
-	
-//	return rooms
-//}
