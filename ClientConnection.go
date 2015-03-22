46a8f2aaacffb5bc4796c98a026206095702b7c8
Randall Hudson <Hudsonrj09@gmail.com>
2015-03-22 01:46:11 -0400

    added event manager to client connection class and had it load the clients character in the description and send the client the first rooms description

--- ClientConnection.go
+++ ClientConnection.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/gob"
+	"fmt"
 	//"fmt"
 	//"io"
 	"net"
@@ -14,38 +15,52 @@ type ClientConnection struct {
 	myDecoder *gob.Decoder
 	net_lock  sync.Mutex
 	character *Character
+	CurrentEM *EventManager
 }
 
 //CliecntConnection constructor
-func newClientConnection(conn net.Conn) *ClientConnection {
+func newClientConnection(conn net.Conn, em *EventManager) *ClientConnection {
 	cc := new(ClientConnection)
 	cc.myConn = conn
+	fmt.Println("Connection: ", conn)
 	cc.myEncoder = gob.NewEncoder(conn)
 	cc.myDecoder = gob.NewDecoder(conn)
 
 	//This associates the clients character with their connection
 	var clientResponse ClientMessage
 	err := cc.myDecoder.Decode(&clientResponse)
-	checkError(err)
+	checkError(err) //TODO replace check errors with somthing that doesnt crash server
 
 	cc.character = newCharacterFromName(clientResponse.Value)
+	cc.CurrentEM = em
+
+	startingRoomDescription := worldRoomsG[cc.character.RoomIN].getRoomDescription()
+	err = cc.myEncoder.Encode(ServerMessage{Value: startingRoomDescription})
+	checkError(err)
 
 	return cc
 }
 
-func (cc *ClientConnection) receiveMsgFromClient(em *EventManager) error {
+func (cc *ClientConnection) receiveMsgFromClient() {
 	var clientResponse ClientMessage
+	fmt.Println("About to begin read loop in receiveMsgFromClient")
+	for {
+		cc.net_lock.Lock()
+		fmt.Println("Aquired lock in receiveMsgFromClient")
+		err := cc.myDecoder.Decode(&clientResponse)
+		cc.net_lock.Unlock()
+		fmt.Println("Released lock in receiveMsgFromClient")
+		//checkError(err)
 
-	cc.net_lock.Lock()
-	err := cc.myDecoder.Decode(&clientResponse)
-	cc.net_lock.Unlock()
-	//checkError(err)
-
-	if err == nil {
-		em.receiveMessage(clientResponse)
+		if err == nil {
+			fmt.Println("Received message from client: ", clientResponse)
+			cc.CurrentEM.receiveMessage(clientResponse)
+		} else {
+			break
+		}
 	}
 
-	return err
+	fmt.Println("After read loop in receiveMsgFromClient")
 }
 
 func (cc *ClientConnection) sendMsgToClient(msg ServerMessage) {
@@ -54,4 +69,9 @@ func (cc *ClientConnection) sendMsgToClient(msg ServerMessage) {
 	err := cc.myEncoder.Encode(msg)
 	checkError(err)
 	//cc.net_lock.Unlock()
+}
+
+func (cc *ClientConnection) setCurrentEventManager(em *EventManager) {
+	cc.CurrentEM = em
+
 }
