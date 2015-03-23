67c64fead780916de7b71245e789f26422d0094f
Randall Hudson <Hudsonrj09@gmail.com>
2015-03-23 05:54:23 -0400

    moved functions around

--- Monster.go
+++ Monster.go
@@ -2,28 +2,26 @@
 package main
 
 import (
-	"math/rand"
-	"io/ioutil"
 	"encoding/xml"
-	"os"
 	"github.com/daviddengcn/go-colortext"
+	"io/ioutil"
+	"math/rand"
+	"os"
+	"time"
 )
+
 type Monster struct {
-	Name string
-	HP int	
-	Defense int
+	Name        string
+	HP          int
+	Defense     int
 	description string
+	targets     map[string]target //TODO add to constructor
+	em          EventManager
 }
 
-type MonsterXML struct {
-	Name string `xml:"Name"`
-	HP int `xml:"HP"`
-	Defense int `xml:"Defense"`
-	Description string `xml:"Description"`
-}
-type MonstersXML struct {
-	XMLName xml.Name `xml:"Monsters"`
-	Monsters []MonsterXML `xml:"Monster"`
+type target struct {
+	aggro        int
+	attackTarget *Character
 }
 
 var monsterTemplatesG map[string]*Monster
@@ -48,29 +46,81 @@ func newMonsterFromName(name string) *Monster {
 	return m
 }
 
+func (m *Monster) fightPlayers() {
+	for {
+		time.Sleep(6 * time.Second)
+
+		if m.HP <= 0 || len(m.targets) <= 0 {
+			break
+		}
+
+		//Find target with highest aggro
+		var attackTarget *Character
+		maxAggro := 0
+		for _, targ := range m.targets {
+			if targ.aggro > maxAggro {
+				attackTarget = targ.attackTarget
+			}
+
+		}
+		event := newEvent(MONSTER, m, "attack", attackTarget.Name, nil)
+		m.em.addEvent(event)
+	}
+}
+
+//TODO implement monsters combat functions
+func (m *Monster) attackPlayer(charName string) {
+
+}
+
 func (m *Monster) getAttackRoll() int {
 	return rand.Int() % 6
 }
 
+func (m *Monster) takeDamage(amount int, typeOfDamge int) []FormattedString {
+	return nil
+}
+
+func (m *Monster) getDefense() int {
+	return -1
+}
+
+func (m *Monster) makeAttack(targetName string) []FormattedString {
+	return nil
+}
+
 func (m *Monster) getLookDescription() []FormattedString {
 	output := make([]FormattedString, 1, 1)
-	
+
 	output[0].Color = ct.Yellow
 	output[0].Value = m.description
-	
+
 	return output
+}
+
+//------------------Loading Stuff------------------------------
+type MonsterXML struct {
+	Name        string `xml:"Name"`
+	HP          int    `xml:"HP"`
+	Defense     int    `xml:"Defense"`
+	Description string `xml:"Description"`
+}
+
+type MonstersXML struct {
+	XMLName  xml.Name     `xml:"Monsters"`
+	Monsters []MonsterXML `xml:"Monster"`
 }
 
 func loadMonsterData() {
 	xmlFile, err := os.Open("monsterData.xml")
 	checkError(err)
 	defer xmlFile.Close()
-	
+
 	XMLdata, _ := ioutil.ReadAll(xmlFile)
 
 	var monstersData MonstersXML
-    xml.Unmarshal(XMLdata, &monstersData)
-	
+	xml.Unmarshal(XMLdata, &monstersData)
+
 	for _, element := range monstersData.Monsters {
 		monsterTemplatesG[element.Name] = newMonsterFromXML(element)
 	}
