ad428898689da907968a1bdd5b7cb9d82c5d7dd8
Randall Hudson <Hudsonrj09@gmail.com>
2015-04-03 19:37:03 -0400

    added some fields to Agent and some methods to Agenter

--- Agent.go
+++ Agent.go
@@ -1,15 +1,27 @@
 package main
 
 type Agent struct {
-	Name           string
-	Health         int
-	RoomIn         int
-	PersonalInv    Inventory
-	EquippedArmour ArmourSet
+	Name         string
+	currentHP    int
+	MaxHitPoints int
+	RoomIN       int
+	//	PersonalInv    Inventory
+	//	EquippedArmour ArmourSet
+
+	//Core Stats
+	Strength     int
+	Constitution int
+	Dexterity    int
+	Wisdom       int
+	Charisma     int
+	Inteligence  int
 }
 
 //The go community says to end interface names with "er"
 type Agenter interface {
-	makeAttack(targetName string) []FormattedString
+	makeAttack(target Agenter) []FormattedString
 	takeDamage(amount int, typeOfDamge int) []FormattedString
+	getName() string
+	getDefense() int
+	isDead() bool
 }
