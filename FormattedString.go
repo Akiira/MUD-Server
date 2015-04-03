afe725388f08bf3de5ad542a916abd4bb3ed00c1
Randall Hudson <Hudsonrj09@gmail.com>
2015-04-03 19:04:05 -0400

    added a help class and some hepler functions to make creating formmatted messages easier. This needs some reorganization.

--- FormattedString.go
+++ FormattedString.go
@@ -5,18 +5,45 @@ import (
 	"github.com/daviddengcn/go-colortext"
 )
 
+type FmtStrCollection struct {
+	fmtedStrings []FormattedString
+	currentColor ct.Color
+}
+
+func parseStringForFormatString(fmtstring string) {
+
+}
+
+func newFormattedStringCollection() *FmtStrCollection {
+	fsc := new(FmtStrCollection)
+	fsc.fmtedStrings = make([]FormattedString, 0, 1)
+	fsc.currentColor = ct.White
+	return fsc
+}
+
+func (fsc *FmtStrCollection) addMessage(color ct.Color, msg string) {
+	fsc.fmtedStrings = append(fsc.fmtedStrings, newFormattedString2(color, msg))
+}
+
 type FormattedString struct {
 	Color ct.Color
 	Value string
 }
 
-func newFormattedString(msg string) []FormattedString {
+func newFormattedString(msg string) FormattedString {
+	return FormattedString{Color: ct.White, Value: msg}
+}
+func newFormattedString2(color ct.Color, msg string) FormattedString {
+	return FormattedString{Color: color, Value: msg}
+}
+
+func newFormattedStringSplice(msg string) []FormattedString {
 	fs := make([]FormattedString, 0, 1)
 	fs = append(fs, FormattedString{Color: ct.White, Value: msg})
 	return fs
 }
 
-func newFormattedString2(color ct.Color, msg string) []FormattedString {
+func newFormattedStringSplice2(color ct.Color, msg string) []FormattedString {
 	fs := make([]FormattedString, 0, 1)
 	fs = append(fs, FormattedString{Color: color, Value: msg})
 	return fs
