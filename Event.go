6ef6b6af0ee9a73c040f11a2c983305f0c6b154b
Randall Hudson <Hudsonrj09@gmail.com>
2015-04-05 15:41:11 -0400

    removed uneeded boolean field

--- Event.go
+++ Event.go
@@ -6,24 +6,22 @@ const (
 )
 
 type Event struct {
-	playerORMonster bool
-	action          string
-	valueOrTarget   Agenter //TODO consider changing this to an Ageneter as well
-	agent           Agenter
-	client          *ClientConnection
+	action string
+	target Agenter //TODO consider changing this to an Ageneter as well
+	agent  Agenter
+	client *ClientConnection
 }
 
 func newEventFromMessage(msg ClientMessage, agent Agenter, cc *ClientConnection) Event {
 	target := eventManager.worldRooms[cc.getCharactersRoomID()].getMonster(msg.Value)
-	return newEvent(PLAYER, agent, msg.Command, target, cc)
+	return newEvent(agent, msg.Command, target, cc)
 }
 
-func newEvent(playerORMonster bool, agent Agenter, action string, target Agenter, cc *ClientConnection) Event {
+func newEvent(agent Agenter, action string, target Agenter, cc *ClientConnection) Event {
 	event := new(Event)
-	event.playerORMonster = playerORMonster
 	event.agent = agent
 	event.action = action
-	event.valueOrTarget = target
+	event.target = target
 	event.client = cc
 	return *event
 }
