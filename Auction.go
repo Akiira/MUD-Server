804fd1bbedf60c9b08274c80c904bc8ec21d765c
Randall Hudson <Hudsonrj09@gmail.com>
2015-04-06 04:37:19 -0400

    implemented a good bit of the new auction thing.

---
+++ Auction.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"github.com/daviddengcn/go-colortext"
+	"time"
+)
+
+//TODO just messing around with interfaces, prolly wont keep it
+type Auctioner interface {
+	newAuction(item *Item, startTime time.Time)
+	beginAuction()
+	sendAuctionInfoToWorld()
+	bidOnItem(amount int, bidder *ClientConnection, timeOfBid time.Time) []FormattedString
+	determineWinner()
+	awardItemToWinner()
+}
+
+type Auction struct {
+	highestBid *Bid //TODO think about best type for this
+	itemUp     *Item
+	endTime    time.Time
+	recentBids []*Bid
+}
+
+type Bid struct {
+	bidder          *ClientConnection
+	durationFromEnd time.Duration
+	amount          int
+}
+
+func (a *Auction) bidOnItem(amount int, bidder *ClientConnection, timeOfBid time.Time) []FormattedString {
+	bid := new(Bid)
+	bid.bidder = bidder
+
+	estimatedTime := timeOfBid.Add(-1 * bidder.getAverageRoundTripTime())
+
+	bid.durationFromEnd = a.endTime.Sub(estimatedTime)
+
+	msg := "Your bid was recorded for time: " + estimatedTime.String() + "\n"
+	return newFormattedStringSplice2(ct.Green, msg)
+}
